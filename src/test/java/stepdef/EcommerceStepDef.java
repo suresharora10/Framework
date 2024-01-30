7bf9ff367857856fe3d925992b8bac967934889b
suresharora10 <suresharora10@gmail.com>
2024-01-30 14:47:52 -0700

    verify auto trigger

--- src/test/java/stepdef/EcommerceStepDef.java
+++ src/test/java/stepdef/EcommerceStepDef.java
@@ -8,27 +8,27 @@ public class EcommerceStepDef {
     @Given("user logins to url {string}")
     public void user_logins_to_url(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("enters email id {string}")
     public void enters_email_id(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("clicks continue")
     public void clicks_continue() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("enters paswword {string}")
     public void enters_paswword(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("clicks sign in")
     public void clicks_sign_in() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        
     }
     @Then("user is on Amazon web page")
     public void user_is_on_amazon_web_page() {
